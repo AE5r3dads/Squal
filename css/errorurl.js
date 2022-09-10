ed3dd0b482723ca66642e3482a8026ddde1d068e
AE5r3dads <105577795+AE5r3dads@users.noreply.github.com>
2022-09-10 18:56:46 +0200

    Add files via upload

---
+++ css/errorurl.js
@@ -0,0 +1,12 @@
+const code = document.getElementById("code")
+console.log(code)
+ErrorEvent(URIError)
+
+URIError(){
+    if()
+        function code{
+            Error(SharedWorker())
+            ShadowRoot(code)
+            SpeechSynthesisVoice
+        }
+    }
